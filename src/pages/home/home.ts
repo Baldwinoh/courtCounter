d6f7700be36ef620f5fed7c42a51a87f62f9c113
Baldwinoh <baldwinoh@gmail.com>
2018-07-11 11:54:27 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WinnerPage } from '../winner/winner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ 
+  scoreForTeamA = 0;
+  scoreForTeamB = 0;
 
+  teamA(value: number): void {
+    this.scoreForTeamA += value;
+
+  }
+
+
+  teamB(value: number): void {
+    this.scoreForTeamB += value;
+
+  }
+
+reset():void{
+  this.scoreForTeamA=0;
+  this.scoreForTeamB =0;
+}
+endMatch(): void{
+  this.navCtrl.push(WinnerPage)
+
+}
   constructor(public navCtrl: NavController) {
 
   }
